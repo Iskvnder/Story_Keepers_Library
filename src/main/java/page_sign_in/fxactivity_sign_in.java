99b1efacf99b229caccd81c231a61fbb362ab7ad
Iskandar Rassulov <iskanderesz@gmail.com>
2022-12-14 08:21:52 +0600

    Main Page Buttons

--- src/main/java/page_sign_in/fxactivity_sign_in.java
+++ src/main/java/page_sign_in/fxactivity_sign_in.java
@@ -3,11 +3,8 @@ package page_sign_in;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
-import javafx.scene.image.Image;
 import javafx.stage.Stage;
-
 import java.io.IOException;
-import java.util.Objects;
 
 public class fxactivity_sign_in extends Application {
 
